26e26c266121b5bd8acee8c6be4b8fc23564e8ea
kikecastillocolombia <enriquecastiyosoy@gmail.com>
2024-10-23 21:47:05 -0500

    primero

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard/Customers',
+};
+function page() {
+  return (
+    <div>
+      customers
+    </div>
+  )
+}
+
+export default page
